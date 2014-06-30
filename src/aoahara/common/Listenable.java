0bff752cdaaf4be0f66fff114e1e1b341f16f4f0
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-06-29 20:13:04 -0400

    Add Listenable Class

---
+++ src/aoahara/common/Listenable.java
@@ -0,0 +1,25 @@
+package aoahara.common;
+
+import java.util.LinkedList;
+
+public abstract class Listenable<T> {
+	
+	private final LinkedList<T> listeners = new LinkedList<>();
+	
+	public void addListener(T listener){
+		listeners.add(listener);
+	}
+	
+	public void clearListeners(){
+		listeners.clear();
+	}
+	
+	public void removeListener(T listener){
+		listeners.remove(listener);
+	}
+	
+	protected java.util.Collection<T> getListeners(){
+		return new LinkedList<T>(listeners);
+	}
+
+}
