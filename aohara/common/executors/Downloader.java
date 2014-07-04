d2faba68a74afd9452abf2a8053551da60de50b2
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-07-03 23:55:01 -0400

    Add Extra constructor to Downloader

--- aohara/common/executors/Downloader.java
+++ aohara/common/executors/Downloader.java
@@ -7,7 +7,11 @@ import java.util.concurrent.Future;
 public class Downloader extends FileExecutor {
 
 	public Downloader(int numThreads) {
-		super(numThreads, FileExecutor.INCREMENT, true);
+		this(numThreads, true);
+	}
+	
+	public Downloader(int numThreads, boolean useTempFile){
+		super(numThreads, FileExecutor.INCREMENT, useTempFile);
 	}
 
 	public Future<Path> download(URL url, Path path) {
