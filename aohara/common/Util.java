df919d973d379bd80cd48a239d6955c1d228e025
Andrew O'Hara <ohara.andrew314@gmail.com>
2015-04-05 15:43:02 -0400

    Add String join helper

--- aohara/common/Util.java
+++ aohara/common/Util.java
@@ -4,6 +4,9 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Class containing utility methods that do not yet belon anywhere.
@@ -22,4 +25,25 @@ public class Util {
         }
 	}
 
+	public static String joinStrings(Object[] strings, String joinOn){
+		return joinStrings(Arrays.asList(strings), joinOn);
+	}
+	
+	public static String joinStrings(Collection<Object> strings, String joinOn){
+		Iterator<Object> iterator = strings.iterator();
+		StringBuilder builder = new StringBuilder();
+		
+		// Append first string if it exists
+		if (iterator.hasNext()){
+			builder.append(iterator.next());
+		}
+		
+		// For all other strings, append them, with the joinOn character preceeding them
+		while (iterator.hasNext()){
+			builder.append(joinOn);
+			builder.append(iterator.next());
+		}
+		
+		return builder.toString();
+	}
 }
